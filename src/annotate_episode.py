21f8be4ebcb3eeb5ecddae9b6bd411a51e1577dd
AndreLamurias <alamurias@lasige.di.fc.ul.pt>
2020-08-30 22:13:28 +0100

    annotate episode and full season

---
+++ src/annotate_episode.py
@@ -0,0 +1,48 @@
+import sys
+import tagme
+import wikipedia
+import spacy
+nlp = spacy.load("en_core_web_sm")
+
+# Set the authorization token for subsequent calls.
+with open("tagme_token", 'r') as f:
+    tagme.GCUBE_TOKEN = f.read().strip()
+
+
+
+def annotate(text):
+    person_entities = []
+    parsed_text = nlp(text)
+    # Get person entities only
+    for e in parsed_text.ents:
+        if e.label_ == "PERSON":
+            if e.text not in person_entities:
+                person_entities.append(e.text)
+    print(person_entities)
+    # TODO: remove series character names
+    entity_text = ", ".join(list(person_entities))
+    entities = []
+    episode = tagme.annotate(entity_text)
+    # Use annotations with a score higher than 0.1
+    for ann in episode.get_annotations(0.1):
+        eid = ann.entity_id
+        ename = ann.entity_title
+        escore = ann.score
+        try:
+            page = wikipedia.page(pageid=eid, preload=True)
+            page_categories = page.categories
+            if any([c.endswith("births") for c in page_categories]):
+                page_summary = page.summary
+                print(escore, eid, ename, ann.mention, page_categories[0])
+                entities.append({"name": ename, "qid": eid, "categories": page_categories,
+                                 "score": escore, "summary": page_summary, "links": page.links,
+                                 "mention": ann.mention})
+        except:
+            print("error getting", ann, eid)
+            continue
+    return entities
+
+if __name__ == "__main__":
+    with open(sys.argv[1]) as f:
+        script = f.read()
+    annotate(script)
